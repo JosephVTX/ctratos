b807a115062c9b16016f262d0e71779c5d90d62f
JosephVTX <jvcallupe69@gmail.com>
2023-08-16 03:54:05 -0500

    avance

--- resources/js/Templates/Contratos/CortoPlazo.tsx
+++ resources/js/Templates/Contratos/CortoPlazo.tsx
@@ -7,6 +7,10 @@ type Props = {
     logo?: boolean;
     nombres: string;
     apellidos: string;
+    capital: string;
+    tipo_cuenta_cliente: string;
+    numero_cuenta_cliente: string;
+    banco_cliente: string;
 };
 
 const Miss = ({ title }: { title: string }) => (
@@ -14,7 +18,15 @@ const Miss = ({ title }: { title: string }) => (
 );
 
 export default function CortoPlazo(props: Props) {
-    const { nombres, apellidos, logo } = props;
+    const {
+        nombres,
+        apellidos,
+        logo,
+        capital,
+        tipo_cuenta_cliente,
+        numero_cuenta_cliente,
+        banco_cliente,
+    } = props;
     return (
         <div>
             <Paper title="CONTRATO N° D19-08-1025 / 2023- GJG" logo={logo}>
@@ -62,12 +74,12 @@ export default function CortoPlazo(props: Props) {
                 </Clausula>
                 <Clausula clausula="Segunda" subtitle="Objeto">
                     <SubClausula sectionNumber="2.1">
-                        El presente contrato tiene por objeto la voluntad de <b>EL
-                        INVERSIONISTA</b> en realizar el financiamiento con un
-                        aporte de capital dentro de los proyectos que <b>EL
-                        INVERSOR</b> tiene o está proyectando realizar, generándole
-                        a <b>EL INVERSIONISTA</b> una rentabilidad que las partes
-                        pactan en el presente contrato.
+                        El presente contrato tiene por objeto la voluntad de{" "}
+                        <b>EL INVERSIONISTA</b> en realizar el financiamiento
+                        con un aporte de capital dentro de los proyectos que{" "}
+                        <b>EL INVERSOR</b> tiene o está proyectando realizar,
+                        generándole a <b>EL INVERSIONISTA</b> una rentabilidad
+                        que las partes pactan en el presente contrato.
                     </SubClausula>
                     <SubClausula sectionNumber="1.2" title="EL INVERSIONISTA">
                         es una persona natural, docente universitario, por el
@@ -79,23 +91,26 @@ export default function CortoPlazo(props: Props) {
                         presente contrato.
                     </SubClausula>
                 </Clausula>
-                <Clausula clausula="Primera" subtitle="Antecedentes">
-                    <SubClausula sectionNumber="1.1" title="EL INVERSOR">
-                        en una persona jurídica dedicada a la asesoría y
-                        consultoría empresarial en al ámbito financiero y a la
-                        gestión de proyectos ya sea en el sector público o
-                        privado, nacional e internacional, a corto, mediano y
-                        largo plazo, realizando inversiones con financiamiento
-                        propio y/o de terceros.
+                <Clausula
+                    clausula="TERCERA"
+                    subtitle="MONTO DE INVERSIÓN Y RENTABILIDAD"
+                >
+                    <SubClausula sectionNumber="3.1">
+                        Mediante la presente, EL INVERSIONISTA aporta al
+                        financiamiento de los proyectos de inversión que
+                        ejecutará EL INVERSOR con un monto ascendente a la suma
+                        de {capital}( Cinco Mil Con 00/100 soles), la cual fue
+                        depositada en la cuenta {tipo_cuenta_cliente} N.º{" "}
+                        {numero_cuenta_cliente} del {banco_cliente}.
                     </SubClausula>
-                    <SubClausula sectionNumber="1.2" title="EL INVERSIONISTA">
-                        es una persona natural, docente universitario, por el
-                        cual percibe una contraprestación, que acumulada en el
-                        tiempo constituye el monto de lo que pretende invertir
-                        en el presente contrato, declarando bajo juramento la
-                        procedencia lícita de dicho fondo, tal como consta en la
-                        declaración jurada que forma parte integrante del
-                        presente contrato.
+                    <SubClausula sectionNumber="3.2">
+                        Como contraprestación del financiamiento que
+                        voluntariamente realiza <b>EL INVERSIONISTA,</b> <b>EL INVERSOR</b> {" "}
+                        entregará el monto de S/ 5,750.00 (Cinco Mil Setecientos
+                        Cincuenta Con 00/100 soles) cuyo monto total será
+                        depositado en la cuenta ahorro N° 163-3132563126 / CCI:
+                        003-163-013132563126-38 a nombre el señor RICARDO JORGE
+                        SEVILLANO SINCA del Banco Interbank .
                     </SubClausula>
                 </Clausula>
             </Paper>
